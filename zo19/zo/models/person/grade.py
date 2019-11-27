bfc1443de39e95094a54d87b6962ff1eb57247ab
Michael Atheros 13 <m.t.atheros@gmail.com>
2019-11-27 15:46:14 +1300

    Work 27/11

--- zo19/zo/models/person/grade.py
+++ zo19/zo/models/person/grade.py
@@ -1,31 +1,9 @@
 from django.db import models
-from djangoyearlessdate.models import YearlessDateField
+from django.core.validators import MinValueValidator, MaxValueValidator
+from djangoyearlessdate.models import YearlessDate
 
 from .gender import Gender
-from zo.models.abstract.base_rank import *
-
-
-class AgeGradeManager(models.Manager):
-
-    def build_all(self):
-
-        ''' Creates every possible permutation of AgeGrade objects, and 
-        fills the database. '''
-
-        # Open Grade
-        if not AgeGrade.objects.all().filter(open=True):
-            AgeGrade(open=True).save()
-
-        # All dd/mm 
-        months = [31,29,31,30,31,30,31,31,30,31,30,31]
-        for i in range(len(months)):
-            m = i+1
-            for d in range(1,i+1):
-                
-                # get all AgeGrades that have this date
-                # check if under=True
-                # create under/over versions                
-                pass              
+from zo.models.abstract.base_rank import *   
 
 class AgeGrade(models.Model):
 
@@ -36,23 +14,13 @@ class AgeGrade(models.Model):
 
     open = models.NullBooleanField(default=False)
     under = models.NullBooleanField(default=None)
-    age = models.PositiveIntegerField(blank=True)
-    date = YearlessDateField(blank=True)
+    age = models.PositiveIntegerField(null=True, default=None)
+    date_day = models.PositiveSmallIntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(31)])
+    date_month = models.PositiveSmallIntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(12)])
 
-    manager = AgeGradeManager()
+    def filter(self, dob):
 
-    def __str__(self):
-
-        if self.open == True:
-            return 'Open'
-        if self.under == None:
-            return '%s Years Old' % self.age
-        elif self.under == True:
-            return 'Under %s' % self.age
-        else:
-            return 'Over %s' % self.age
-
-
+        pass
 
 class RankGroupType(BaseRankGroupType):
 
@@ -83,22 +51,6 @@ class Rank(BaseRank):
     # rank_value
     rank_group = models.ForeignKey(RankGroup, on_delete=models.CASCADE, related_name='ranks')
 
-
-class GradeManager(models.Manager):
-
-    def build_all(self):
-
-        ''' Creates every possible permutation of Grade objects, and 
-        fills the database. '''
-        
-        AgeGrade.build_all()
-
-        # recursive functions?
-
-        genders = Gender.objects.all()
-        ranks = Rank.objects.all()
-        age_grades = AgeGrade.objects.all()
-      
 class Grade(models.Model):
 
     ''' A collection of one or more age, gender and/or rank filters. '''
@@ -106,8 +58,6 @@ class Grade(models.Model):
     age = models.ForeignKey(AgeGrade, null=True, on_delete=models.CASCADE, related_name='grades')
     genders = models.ManyToManyField(Gender, related_name='grades')
     ranks = models.ManyToManyField(Rank, related_name='grades')
-
-    manager = GradeManager()
 
     def __str__(self):
 
--- zo19/zo/seed/person.py
+++ zo19/zo/seed/person.py
@@ -2,6 +2,7 @@
 
 from zo.models.person.gender import Gender
 from zo.models.person.grade import AgeGrade, RankGroupType, RankGroup, Rank, Grade
+from djangoyearlessdate.models import YearlessDate
 
 from datetime import timedelta, date
 
@@ -45,28 +46,32 @@ class SeedPersonModels():
         else:
             open = AgeGrade(open=True)
             open.save()
-        self.age_grades.append(open)
 
         # create yearless_dates
         yearless_dates = []
-        def daterange(start_date, end_date):
-            for n in range(int ((end_date - start_date).days)):
-                yield start_date + timedelta(n)
-        for single_date in daterange(date(2020, 1, 1), date(2021, 1, 1)):
-            yearless_dates.append(single_date.strftime("%m/%d"))
+        for m in range(1, 13):
+            for d in range(1, 32):
+                try:
+                    YearlessDate(d, m)
+                    yearless_dates.append((d,m))
+                except:
+                    pass
 
         # dateless age, under and over age with yearless_date
-        if len(AgeGrade.objects.filter(age=13)) == 0:
-            for a in range(0, 121):
+        for a in range(0, 121):
+
+            if len(AgeGrade.objects.filter(age=a).filter(under=None)) == 0:
                 ag = AgeGrade(age=a)
                 ag.save()
-                self.age_grades.append(ag)
 
-                for under in [True, False]:
-                    for date in yearless_dates:
-                        yless_ag = AgeGrade(age=a, under=under, date=date)
+            for under in [True, False]:
+                for date in yearless_dates:
+                    if len(AgeGrade.objects.filter(age=a).filter(under=under).filter(date_day=date[0]).filter(date_month=date[1])) == 0:
+                        yless_ag = AgeGrade(age=a, under=under, date_day=date[0], date_month=date[1])
                         yless_ag.save()
-                        self.age_grades.append(yless_ag)              
+                        
+        self.age_grades = AgeGrade.objects.all()
+        return
 
     def seed_ranks(self):
 
@@ -75,26 +80,33 @@ class SeedPersonModels():
         rgt_age = RankGroupType.objects.filter(name='Age')
         if len(rgt_age) == 0:
             rgt_age = RankGroupType(name='Age', description='Rank Groups that have a general "age" aspect to their ranking.')
+            rgt_age.save()
         else:
             rgt_age = rgt_age[0]
 
         rg_nz = RankGroup.objects.filter(name='NZ School Year Levels')
         if len(rg_nz) == 0:
             rg_nz = RankGroup(name='NZ School Year Levels',
-                                description='The NZ primary and secondary school year levels, generally for students aged around 5 - 18')                
+                                description='The NZ primary and secondary school year levels, generally for students aged around 5 - 18')
+            rg_nz.save()
+            rg_nz.types.add(rgt_age)
         else:
             rg_nz = rg_nz[0]
-            rg_nz.types.add(rgt_age)
 
         for i in range(1,14):
 
-            if Rank.objects.filter(name='NZ School Year Levels').filter(value=i):
+            if Rank.objects.filter(name='Year %s' % i).filter(rank_value=i):
                 continue
 
             name = 'Year %s' % i
-            value = i
-            Rank(name=name, rank_value=rank, rank_group=rg_nz).save()
+            Rank(name=name, rank_value=i, rank_group=rg_nz).save()
+
+        self.ranks = Rank.objects.all()
 
     def seed_grades(self):
+
+        '''Genders can be multi, ranks can be multi 
+        (though as all current ranks are age based, they will not occur with age_grades), 
+        age_grades are only 1 '''
 
         pass
